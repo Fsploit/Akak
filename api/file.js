721a6c14ec1d18c78fe9dc693930fcbe07b620cb
CRINGE_X3 <169143383+Fsploit@users.noreply.github.com>
2026-02-28 15:13:01 +0800

    Update file.js

--- api/file.js
+++ api/file.js
@@ -1,30 +1,16 @@
 export default function handler(req, res) {
     try {
         const hosted = req.query.hosted;
-        const userAgent = req.headers['user-agent'] || '';
 
         // 1. Key Authentication
         if (hosted !== "bee54fbc56a2a65809c4519b3816cff770e45d072d4dd53e1675f865beb9f6d7") {
             return res.status(403).send("You're not allowed to see this file contents.");
         }
 
-        // 2. Anti-Browser Check
-        // If the request looks like it's coming from a standard web browser, hide the real script.
-        const isBrowser = /Mozilla|Chrome|Safari|Edge|Opera/i.test(userAgent);
-        if (isBrowser) {
-            // Return a decoy script or a denial message.
-            res.setHeader("Content-Type", "text/plain");
-            return res.status(200).send(`I wanna say for you all skidders, that this code is obfuscated by LURAPH!! yes. luraph so dont even try to deobfuscate, i knoy you guys aint doing shit.`);
-        }
-
-        // 3. Serve the Real (Obfuscated) Script
+        // 2. Serve the Script
         res.setHeader("Content-Type", "text/plain");
         
-        // PASTE YOUR OBFUSCATED LUA SCRIPT HERE
-        // Do not put raw, readable Lua here if you want it truly hidden.
-        return res.status(200).send(`
-print("hewwwo")
-        `);
+        return res.status(200).send(`print("hi")`);
 
     } catch (err) {
         return res.status(500).send("Server Error");
