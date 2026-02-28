3d5918d17d42d3fc95d57b6dc0ab9029039f8ce0
CRINGE_X3 <169143383+Fsploit@users.noreply.github.com>
2026-02-28 14:47:41 +0800

    Refactor handler to include error handling

--- api/file.js
+++ api/file.js
@@ -1,14 +1,19 @@
 export default function handler(req, res) {
-    const content = req.query.content;
+    try {
+        const hosted = req.query.hosted;
 
-    if (hosted !== "bee54fbc56a2a65809c4519b3816cff770e45d072d4dd53e1675f865beb9f6d7") {
-        res.status(403).send("You're not allowed to see this file contents.");
-        return;
+        if (hosted !== "bee54fbc56a2a65809c4519b3816cff770e45d072d4dd53e1675f865beb9f6d7") {
+            return res.status(403).send("You're not allowed to see this file contents.");
+        }
+
+        res.setHeader("Content-Type", "text/plain");
+
+        return res.status(200).send(`
+            print("Script executed successfully")
+            -- Your real Lua script here
+        `);
+
+    } catch (err) {
+        return res.status(500).send("Server Error");
     }
-
-    res.setHeader("Content-Type", "text/plain");
-
-    res.status(200).send(`
-        print("hi")
-    `);
 }
